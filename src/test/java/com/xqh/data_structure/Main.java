c5553a5c41eeaf92c77e4800c2b673eb1dcb1776
redkueen <xqh20186@163.com>
2017-09-11 23:39:34 +0800

    数据结构：添加包名

--- src/test/java/com/xqh/data_structure/Main.java
+++ src/test/java/com/xqh/data_structure/Main.java
@@ -1,4 +1,7 @@
 package com.xqh.data_structure;
+
+import com.xqh.data_structure.dynamic_array.XqhArray;
+
 public class Main {
     public static void main (String[]args){
       XqhArray xa = new XqhArray();
