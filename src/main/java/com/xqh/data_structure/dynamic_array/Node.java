a781567408de88d91349e602cdbd563dea5e45c5
redkueen <xqh20186@163.com>
2017-09-14 21:49:03 +0800

    数据结构： 单向链表的   addFirst()方法(在头部插入元素)   add()   get()    display()     insert()   set() (修改制定下表的元素)  removeLast()  delete() 等方法

---
+++ src/main/java/com/xqh/data_structure/dynamic_array/Node.java
@@ -0,0 +1,10 @@
+package com.xqh.data_structure.dynamic_array;
+
+public class Node {
+    int data0;
+    Node next0;
+   void Node(int data,Node next){
+        this.data0 =data;
+        this.next0 = next;
+    }
+}
---
+++ src/test/java/com/xqh/data_structure/XqhLinkedListMain.java
@@ -0,0 +1,26 @@
+package com.xqh.data_structure;
+
+import com.xqh.data_structure.dynamic_array.XqhLinkedList;
+
+public class XqhLinkedListMain {
+    public static void main(String[]args){
+       XqhLinkedList lit = new XqhLinkedList();
+       lit.addFirst(2);
+       lit.addFirst(5);
+       lit.add(7);
+       lit.add(15);
+       lit.add(9);
+       lit.display();
+       lit.insert(3,18);
+       lit.display();
+       lit.insert(4,37);
+       lit.set(5,30);
+       lit.display();
+       int element1 = lit.get(4);
+       lit.removeLast();
+       lit.removeLast();
+       lit.display();
+       lit.delete(7);
+       lit.display();
+    }
+}
